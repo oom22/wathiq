c4d17590fa3b2978553fab48f10cbba999d70816
SulaimanMj <Randaljarb3@gmail.com>
2025-10-02 12:43:55 +0300

    theme added

---
+++ lib/features/theme/app_colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+
+abstract final class AppColors {
+  // Primary
+  static const Color primary = Color(0xFF113E45);
+
+  // Secondary
+  static const Color secondary1 = Color(0xFF035C6B);
+  static const Color secondary2 = Color(0xFF007793);
+
+  // Backgrounds
+  static const Color background = Color(0xFFEAF8FA);
+  static const Color fieldsAndLogo = Color(0xFFFFFFFF);
+
+  // // Optional text contrast
+  // static const Color onPrimary = Colors.white;
+  // static const Color onBackground = Color(0xFF0F1B1E);
+}
