1602b62f4cdf2b32b54ec5422a0da5f205fab13f
Rager <0ooomar.2002@gmail.com>
2025-10-02 18:03:17 +0300

    splash third time

--- lib/features/navigation/screen/navigation_screen.dart
+++ lib/features/navigation/screen/navigation_screen.dart
@@ -1,7 +1,6 @@
 import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class NavigationScreen extends StatefulWidget {
   const NavigationScreen({super.key});
@@ -28,5 +27,3 @@ class _NavigationScreenState extends State<NavigationScreen> {
     );
   }
 }
-
-//hello
