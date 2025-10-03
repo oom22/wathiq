d56fdf2239a7cf3de253d1cce4d0a3e8e131a135
Rager <0ooomar.2002@gmail.com>
2025-10-03 20:45:24 +0300

    rm annoying import

--- lib/features/navigation/controller/navigation_controller.dart
+++ lib/features/navigation/controller/navigation_controller.dart
@@ -1,13 +1,15 @@
-import 'package:flutter/material.dart';
 import 'package:persistent_bottom_nav_bar/persistent_bottom_nav_bar.dart';
 
 class NavigationController {
-  static final NavigationController _instance = NavigationController._internal();
+  static final NavigationController _instance =
+      NavigationController._internal();
   factory NavigationController() => _instance;
 
   NavigationController._internal();
 
-  final PersistentTabController controller = PersistentTabController(initialIndex: 2);
+  final PersistentTabController controller = PersistentTabController(
+    initialIndex: 2,
+  );
 
   void changeTab(int index) {
     controller.index = index;
